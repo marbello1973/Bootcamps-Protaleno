1b794c9a845a2af24d438b22a5045d8c1ba4c24c
marbello1973 <david.marbello@outlook.com>
2023-05-03 22:32:41 -0500

    lista

---
+++ contact-list/project-1/README.md
@@ -0,0 +1,6 @@
+# Lista de contactos - 1
+* Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
+* Crea una función para añadir un nuevo contacto a una lista
+* Crea una función para borrar un contacto existente de la lista
+* Crea una función para imprimir en consola los contactos presentes en la lista
+* Publica tu código en Github, en tu repositorio contact-list crea un branch llamado project-1 y compártelo con nosotros 👍🏼⬇️
--- contact-list/project-1/contact-list.js
+++ contact-list/project-1/contact-list.js
@@ -1,3 +1,33 @@
+//nota se creo un package json para instalar nodemon
+
+//importar el archivo json con lista usuario con nombre, apellido, id opcional en caso de ser necesario
+const contactos = require("./listaContactos.json");
+
+//Crea una función adicional que me imprima todos los contactos de la lista
+const users = (name) => {
+  name = contactos;
+  let findName = name.map((el) => {
+    return {
+      name: el.name,
+      lastname: el.lastName,
+    };
+  });
+  return findName;
+};
+// console.log(users());
+
+const addUser = (name, lastname, id) => {
+  const user = {
+    name: name,
+    lastname: lastname,
+  };
+  return user;
+};
+
+console.log(addUser());
+
+/*
+
 //creando funcion vacia para con array vacio para ir adicionando los usuarios
 function Contact() {
   this.str = [];
@@ -37,4 +67,4 @@ newUsers.dellUsers("maria");
 console.log(newUsers);
 
 console.log(newUsers.showUsers());
-
+ */
